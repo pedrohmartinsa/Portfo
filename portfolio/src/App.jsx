06de5b440918788f26027508f774f099b5ff8115
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-10-02 22:42:48 -0300

    tentando arrumar o footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,13 +8,14 @@ function App() {
 
   return (
     <>
-    
-      <div className="relative flex items-center justify-center w-full h-screen">
+
+      <div className="relative flex items-center justify-center w-screen h-screen">
 
         <div className="py-10 border-2 border-black w-[95%] h-[90%]">
 
         </div>
-        <div className="absolute bottom-[-35px]">
+
+        <div className="absolute bottom-[-1px] mb-1">
 
         <Footer/>
         </div>
--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -6,12 +6,12 @@ import { MdEmail } from "react-icons/md";
 export default function Footer() {
     return(
         <>
-            <footer className="px-3 flex items-center justify-between py-7 gap-y-10 text-black w-full
-                                md:flex-row md:justify-between md:px-6 ">
+            <footer className="flex justify-between text-black w-full
+                                ">
 
                 <h3 className="text-xs font-bold text-slate-700">Copyright © PH</h3>
 
-                <div className="flex gap-x-5">
+                <div className="flex justify-between gap-x-5">
 
                     <a href="https://www.linkedin.com/in/pedrohmas/">
                         <FaLinkedin color="black"/>
